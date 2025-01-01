7fb4e33ee5b65c0f28e7fdbe68cf977bb98be93a
NationalWind <94291098+NationalWind@users.noreply.github.com>
2025-01-01 17:44:43 +0700

    fix cart

--- SushiWeb/src/backend/routes/cart_routes.js
+++ SushiWeb/src/backend/routes/cart_routes.js
@@ -2,7 +2,8 @@ import express from "express";
 import {
     getCartItems,
     selectCartItem,
-    deleteCartItem
+    deleteCartItem,
+    createOrder
 } from "../controller/cart_controller.js";
 
 const router = express.Router();
@@ -15,5 +16,8 @@ router.post("/select", selectCartItem);
 
 // Route to delete a specific item in the cart
 router.post("/delete", deleteCartItem);
+
+// Route to create a new order
+router.post("/create-order", createOrder);
 
 export default router;
